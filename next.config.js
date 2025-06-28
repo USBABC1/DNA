2f7748e3662b41cd739b87c02eff89f321d92967
USBABC1 <contatousbabc@gmail.com>
2025-06-28 03:04:39 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   experimental: {
   },
   images: {
-    domains: [\'lh3.googleusercontent.com\'],
+    domains: ["lh3.googleusercontent.com"],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
