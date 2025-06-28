7361b7ab6e590624743ae2d29b7961f272d54923
USBABC1 <contatousbabc@gmail.com>
2025-06-28 02:56:03 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */const nextConfig = {
+/** @type {import(\'next\').NextConfig} */
+const nextConfig = {
   experimental: {
   },
-  images:     domains: ['lh3.googleusercontent.com'],
+  images: {
+    domains: [\'lh3.googleusercontent.com\'],
   },
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
